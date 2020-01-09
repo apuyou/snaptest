aad1d28264200b3f685e4e64dd0a181a77e27691
Arthur Puyou <arthur@puyou.fr>
2020-01-09 10:57:59 +0100

    Snapchat button

---
+++ src/snapchat.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+
+const Snapchat = () => {
+  useEffect(() => {
+    // Load the SDK asynchronously
+    // This is the code from Snapchat doc. It handles loading the JS only once.
+    (function(d, s, id) {
+      var js,
+        sjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://sdk.snapkit.com/js/v1/create.js';
+      sjs.parentNode.insertBefore(js, sjs);
+    })(document, 'script', 'snapkit-creative-kit-sdk');
+  }, []);
+
+  return (
+      <div className="snapchat-creative-kit-share"></div>
+  );
+};
+
+export default Snapchat;
