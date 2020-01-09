1f0dabc9ad7a50f08560ba0ccb5858304e91ebcc
Arthur Puyou <arthur@puyou.fr>
2020-01-09 11:06:12 +0100

    Add Open Graph tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
 import './App.css';
 import Snapchat from './snapchat';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta content="Snaptest – Dynamic" property="og:site_name" />
+        <meta
+          content="This tag is generated from JavaScript."
+          property="og:title"
+        />
+      </Helmet>
       <header className="App-header">
         Share this page!
         <Snapchat />
